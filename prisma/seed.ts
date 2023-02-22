bc8389c12a335a83c6d7b8edfd571342ae0c8f0b
kriszi999 <o.kriszi99@gmail.com>
2023-02-22 18:57:38 +0100

    Remove git auth

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,8 @@
-im;
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+
+async function main() {
+  const user1 = await prisma.user.create({
+    data: {},
+  });
+}
